f456009a1aae86102224f3e71390e47c8cccb6dd
James Dow <photodow@gmail.com>
2024-01-23 04:07:20 -0600

    fixing small grid item

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -6,13 +6,13 @@ export default function Header() {
     <header className="jd-header" data-carbon-theme="g10">
       <div className="jd-header__inner cds--grid">
         <div className="cds--row">
-          <div className="cds--col-sm-4 cds--col-md-2">
+          <div className="cds--col-sm-4 cds--col-md-3">
             <h1 className="jd-header__title">
               <span className="jd-header__name">James Dow</span>
               <span className="jd-header__role">Web Designer & Developer</span>
             </h1>
           </div>
-          <div className="cds--col-sm-4 cds--col-md-6">
+          <div className="cds--col-sm-4 cds--col-md-5">
             <Navigation />
           </div>
         </div>
