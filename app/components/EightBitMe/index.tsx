f86874782d08f565f2c53b9d4b800bc416ca6a36
James Dow <photodow@gmail.com>
2024-01-24 17:10:37 -0600

    some cleanup and touch controls

--- app/components/EightBitMe/index.tsx
+++ app/components/EightBitMe/index.tsx
@@ -1,48 +1,70 @@
 "use client";
 
-import { MouseEventHandler, useEffect } from 'react';
+import { MouseEventHandler, useEffect, useState, useRef } from 'react';
+
+// leverage state more
+// slow loadtime
 
 import "./index.scss";
 
 let eightBitMeElement: HTMLElement | null;
 
 export default function EightBitMe({ mobileNavControl }: {mobileNavControl: MouseEventHandler}) {
+  const eightBitMe = useRef(null);
+  const [lookingDirection, setLookingDirection] = useState('left');
+  const [showMe, setShowMe] = useState('');
+
   useEffect(() => {
+    resizeAvatar();
     eightBitMeElement = document.querySelector('.jd-eightbitme__avatar');
-    document.addEventListener('mousemove', setLookingDirection);
     document.addEventListener('scroll', resizeAvatar);
-  });
+
+    setShowMe('jd-eightbitme--active');
+
+
+    document.addEventListener('mousemove', e => {
+      calcLookingDirection(e.x, e.y, eightBitMe.current, setLookingDirection);
+    });
+
+    document.addEventListener("touchmove", e => {
+      const changedTouches = e.changedTouches[0];
+      calcLookingDirection(changedTouches.clientX, changedTouches.clientY, eightBitMe.current, setLookingDirection);
+    });
+  }, []);
 
   return (
-    <div className="jd-eightbitme">
-      <div className="jd-eightbitme__avatar left" onClick={mobileNavControl}>
-        <div className="hair-wind"></div>
-        <div className="eyes"></div>
+    <div className={`jd-eightbitme ${showMe}`}>
+      <div className={`jd-eightbitme__inner`}>
+        <div ref={eightBitMe} className={`jd-eightbitme__avatar ${lookingDirection}`} onClick={mobileNavControl}>
+          <div className="hair-wind"></div>
+          <div className="eyes"></div>
+        </div>
       </div>
+      <div className={`jd-eightbitme__bg`} />
     </div>
   );
 }
 
 let eightBitBox: DOMRect | undefined;
 let originalHeight: number;
-let originalRight: number;
 const minHeight = 42; // header-inner - padding
+const padding = 8;
 
-function resizeAvatar (e: Event) {
-  const eightbitContainer: HTMLElement | null = document.querySelector('.jd-eightbitme');
+function resizeAvatar () {
+  const eightbitContainer: HTMLElement | null = document?.querySelector('.jd-eightbitme');
+  const eightbitBG: HTMLElement | null = document?.querySelector('.jd-eightbitme__bg');
 
-  if (!eightbitContainer) {
+  if (!eightbitContainer || !eightbitBG) {
     return false;
   }
 
   if (!eightBitBox) {
     eightBitBox = eightbitContainer.getBoundingClientRect();
-    originalHeight = eightBitBox.height - 16;
-    originalRight = eightBitBox.right;
+    originalHeight = eightBitBox.height - padding * 2;
     console.log
   }
 
-  eightBitBox.height = originalHeight - window.pageYOffset;
+  eightBitBox.height = originalHeight - window.scrollY;
 
   if (eightBitBox.height <= minHeight) {
     eightBitBox.height = minHeight;
@@ -53,8 +75,10 @@ function resizeAvatar (e: Event) {
   eightbitContainer.style.height = `${eightBitBox.height}px`;
   eightbitContainer.style.fontSize =  `${calcMidPointBy(16, 2.25)}px`;
   eightbitContainer.style.transform = `translate(${calcMidPointBy(50, 0)}%, 0)`;
-  eightbitContainer.style.top = `${calcMidPointBy(0, 8)}px`;
-  eightbitContainer.style.right = `calc(${calcMidPointBy(50, 0)}% + ${calcMidPointBy(0, 8)}px)`;
+  eightbitContainer.style.top = `${calcMidPointBy(0, padding)}px`;
+  eightbitContainer.style.right = `calc(${calcMidPointBy(50, 0)}% + ${calcMidPointBy(0, padding)}px)`;
+
+  eightbitBG.style.transform = `scale(${calcMidPointBy(0, 1)}) translate(-50%, -50%)`;
 }
 
 function calcMidPointBy (
@@ -67,38 +91,25 @@ function calcMidPointBy (
   return ((height - toHeight) / (fromHeight - toHeight) * (from - to)) + to;
 }
 
-function setLookingDirection (e: MouseEvent) {
-  if (!eightBitMeElement) {
-    return false;
+function calcLookingDirection (x: number, y: number, ref: HTMLElement | null, update: Function) {
+  if (!ref) {
+    return;
   }
 
-  const avatarBox = eightBitMeElement.getBoundingClientRect();
+  let direction = [];
+  const avatarBox = ref.getBoundingClientRect();
 
-  if (e.x < avatarBox.left) {
-      // left
-      eightBitMeElement.classList.add('left');
-      eightBitMeElement.classList.remove('right');
-  } else if (e.x > avatarBox.right) {
-      // right
-      eightBitMeElement.classList.add('right');
-      eightBitMeElement.classList.remove('left');
-  } else {
-      // middle
-      eightBitMeElement.classList.remove('right');
-      eightBitMeElement.classList.remove('left');
+  if (x < avatarBox.left) {
+      direction.push('left');
+  } else if (x > avatarBox.right) {
+      direction.push('right');
   }
   
-  if (e.y < avatarBox.top) {
-      // top
-      eightBitMeElement.classList.add('up');
-      eightBitMeElement.classList.remove('down');
-  } else if (e.y > avatarBox.bottom) {
-      // bottom
-      eightBitMeElement.classList.add('down');
-      eightBitMeElement.classList.remove('up');
-  } else {
-      // middle
-      eightBitMeElement.classList.remove('down');
-      eightBitMeElement.classList.remove('up');
+  if (y < avatarBox.top) {
+      direction.push('up');
+  } else if (y > avatarBox.bottom) {
+      direction.push('down');
   }
+
+  update(direction.join(' '));
 }
