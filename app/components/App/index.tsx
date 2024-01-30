11e2ab69aecc05b0805179d5b620919b3113134e
James Dow <photodow@gmail.com>
2024-01-30 01:03:02 -0600

    fixing build

--- app/components/App/index.tsx
+++ app/components/App/index.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 import { getSiteData } from "../../utils/firebase";
 import { SiteDataContext } from "../../utils/contexts";
 import { SiteData } from "../../types/siteData";
-import { useSearchParams } from "next/navigation";
 
 type Comp = {
   id?: string,
@@ -16,13 +15,12 @@ type Comp = {
 }
 
 export default function App({ id, children, miniHeader }: Comp) {
-  const searchParams = useSearchParams();
   const [siteData, setSiteData] = useState<SiteData | null>(null);
 
   useEffect(() => {
-    getSiteData(id || searchParams.get('id')).then(d => setSiteData(d));
-    // handle a 404 scenario incase data doesn't load?
-  }, [searchParams, id]);
+    getSiteData(id || new URLSearchParams(location.search).get('id')).then(d => setSiteData(d));
+    // todo: handle a 404 scenario incase data doesn't load?
+  }, [id]);
 
   return (
     <SiteDataContext.Provider value={siteData}>
