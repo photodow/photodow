fdfc3e4b80f127f94fecc1be9f4959558cded98e
James Dow <photodow@gmail.com>
2024-02-02 01:10:17 -0600

    adding gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.scss";
 
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import GridOverlay from "./_components/GridOverlay";
 import type { Metadata } from 'next'
  
@@ -21,6 +21,7 @@ export default function RootLayout({
         <GridOverlay />
       </body>
       <GoogleAnalytics gaId="G-25V741CH1X" />
+      <GoogleTagManager gtmId="GTM-P7QPPG7W" />
     </html>
   );
 }
