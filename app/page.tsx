53ee2166b09eb457d3a53aeef8c11770f9b5c539
James Dow <photodow@gmail.com>
2024-02-05 02:31:00 -0600

    fixed variable names

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import "./page.scss";
 
 import App from "./_components/App";
 import AboutMe from "./_components/AboutMe";
-import MyWork from "./_components/MyExperience";
-import MyExperience from "./_components/MyWork";
+import MyExperience from "./_components/MyExperience";
+import MyWork from "./_components/MyWork";
 
 export default function Home() {
   return (
     <App>
       <AboutMe />
-      <MyExperience />
+      {/* <MyExperience /> */}
       <MyWork />
     </App>
   );
