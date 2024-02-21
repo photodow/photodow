bb823dad9b7e9ac13889393969c08c5fc5eb5a26
James Dow <photodow@gmail.com>
2024-02-21 11:48:34 -0600

    testing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import "./globals.scss";
 
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import GridOverlay from "./_components/GridOverlay";
-import type { Metadata } from 'next';
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: 'James Dow — Designer & Developer',
-  description: `James Dow works at the intersection of design and development. Collaborating through HTML, CSS, Javascript, and Design Thinking to push an idea from conception to its existence.`,
-}
+const title = `James Dow's | Designer and Developer`;
+const description = `James Dow works at the intersection of design and development for companies like Peacock and IBM. James Dow pushes ideas from conception to their existence. James Dow collaborates using HTML, CSS, Javascript, and Design Thinking.`;
+const socialImage = `https://jamesdow.me/james-dow-experiences.png`;
+const url = 'https://jamesdow.me';
 
 export default function RootLayout({
   children,
@@ -16,6 +16,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={socialImage} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={socialImage} />
+      </Head>
       <body>
         {children}
         <GridOverlay />
