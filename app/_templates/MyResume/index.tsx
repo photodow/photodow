aebdc5a81ff34ac57c7c96ba9d4d61c26bb6bfea
James Dow <photodow@gmail.com>
2024-05-10 14:27:34 -0500

    resume getting data

--- app/_templates/MyResume/index.tsx
+++ app/_templates/MyResume/index.tsx
@@ -2,17 +2,47 @@
 
 import "./index.scss";
 
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { SiteDataContext } from "../../_utils/contexts";
-import { HeaderSize } from "../../_types/Header";
 import App from "../../_components/App";
+import { Experience } from "../../_types/Experience";
+import GetRefs from "../../_utils/getRefs";
+import { RefList } from "../../_types/Ref";
 
 export function MyResumeTemplate() {
     const siteData = useContext(SiteDataContext);
+    const [experiences, setExperiences] = useState<Experience[]>([]);
+
+    const setExperienceData = useCallback(() => {
+        if (siteData) {
+            setExperiences(
+                (
+                    GetRefs(
+                        siteData.main?.experience,
+                        RefList.Experiences,
+                        siteData,
+                    ) as Experience[]
+                ).sort((a, b) => {
+                    const presentYear = new Date().getFullYear();
+                    const bEnd =
+                        typeof b.end === "number" ? b.end : presentYear;
+                    const aEnd =
+                        typeof a.end === "number" ? a.end : presentYear;
+
+                    return b.start - a.start || bEnd - aEnd;
+                }),
+            );
+        }
+    }, [siteData]);
+
+    useEffect(() => {
+        setExperienceData();
+    }, [setExperienceData, siteData]);
 
     return (
-        <App>
+        <>
             <h1>{siteData?.main?.name}</h1>
-        </App>
+            <p>asdf asdf</p>
+        </>
     );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,6 +3,7 @@ import "./index.scss";
 import { Metadata } from "next";
 import { metaDataObj, roleDefault } from "../_utils/metadata";
 import { MyResumeTemplate } from "../_templates/MyResume";
+import App from "../_components/App";
 
 const title = `James Dow's Resume`;
 const description = `Download, print, and review James Dow's ${roleDefault} Resume.`;
@@ -10,5 +11,9 @@ const description = `Download, print, and review James Dow's ${roleDefault} Resu
 export const metadata: Metadata = metaDataObj(title, description);
 
 export default function Resume() {
-    return <MyResumeTemplate />;
+    return (
+        <App>
+            <MyResumeTemplate />
+        </App>
+    );
 }
