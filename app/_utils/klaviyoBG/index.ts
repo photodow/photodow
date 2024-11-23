dd8bb44ee731bc23f82e2671e443bceddde8fe2e
photodow <photodow@gmail.com>
2024-11-22 23:53:25 -0600

    updated

---
+++ app/_utils/klaviyoBG/index.ts
@@ -0,0 +1,77 @@
+import { Loader } from "@googlemaps/js-api-loader";
+import { disableMotion } from "../disableMotion";
+
+let map: google.maps.Map;
+let mapElem: Element | null = null;
+const moveMapBy = 0.000005;
+const latlng = {
+    lat: 28.0882103,
+    lng: -82.4576896,
+};
+
+// var myStyles = [
+//     {
+//         featureType: "all",
+//         elementType: "labels",
+//         stylers: [
+//               { visibility: "off" }
+//         ]
+//     },
+//     {
+//         featureType: "poi",
+//         elementType: "labels",
+//         stylers: [
+//               { visibility: "off" }
+//         ]
+//     }
+// ];
+
+export function initFleetisticsBG() {
+    new Loader({
+        apiKey: "AIzaSyDrktX3psQRb99tv96YY4HxnJf_Tqv-UvY",
+        version: "weekly",
+        //   ...additionalOptions,
+    })
+        .importLibrary("maps")
+        .then((google) => {
+            mapElem = document.querySelector(
+                "#experience-fleetistics .jd-slant__bg",
+            );
+
+            if (!mapElem) {
+                return;
+            }
+
+            map = new google.Map(mapElem as HTMLElement, {
+                center: latlng,
+                zoom: 18,
+                heading: -50,
+                tilt: 150,
+                mapId: "5d1feb3d85ffb2af",
+                disableDefaultUI: true,
+                gestureHandling: "none",
+                // suppressMarkers: true
+                // styles: myStyles
+            });
+        })
+        .catch((e) => {
+            // do something
+        });
+}
+
+export function animateFleetisticsBG() {
+    if (
+        map &&
+        !disableMotion("disableFleetisticsMotion") &&
+        mapElem?.classList.contains("visible")
+    ) {
+        latlng.lat -= moveMapBy;
+        // latlng.lng -= addBy;
+
+        map.setCenter(latlng);
+
+        // map.moveCamera({
+        //     center: latlng
+        // });
+    }
+}
--- app/_utils/sortExperiences.ts
+++ app/_utils/sortExperiences.ts
@@ -2,10 +2,18 @@ import { Experience } from "../_types/Experience";
 
 export function sortExperiences(a: Experience, b: Experience) {
     const presentYear = new Date().getFullYear();
-    const bEndYear = !b.end?.present && b.end?.year ? b.end?.year : presentYear;
-    const aEndYear = !a.end?.present && a.end?.year ? a.end?.year : presentYear;
+    let bEndYear = !b.end?.present && b.end?.year ? b.end?.year : presentYear;
+    let aEndYear = !a.end?.present && a.end?.year ? a.end?.year : presentYear;
     // const bStartYear = b.start?.year || 0;
     // const aStartYear = a.start?.year || 0;
+
+    if (a.end?.present) {
+        aEndYear += 1;
+    }
+
+    if (b.end?.present) {
+        bEndYear += 1;
+    }
 
     return bEndYear - aEndYear;
     // return bStartYear - aStartYear || bEndYear - aEndYear;
